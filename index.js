1dd68166774a84d1cbdd56f357219d3f74f37401
Adalberto "Nick" Garza <angarza@alum.mit.edu>
2018-03-14 16:24:44 -0400

    First commit

---
+++ index.js
@@ -0,0 +1,20 @@
+/*gloval $ */
+//https://talaikis.com/random_quotes_api/
+
+function getQuote () {
+	$.ajax({
+		method:"GET",
+		url:"https://talaikis.com/api/quotes/random/",
+		success: function(data){
+			console.log(data);
+			document.getElementById("insertQuote").innerHTML = data.quote;
+			document.getElementById("insertAuthor").innerHTML = data.author;
+	}
+});
+}
+
+document.getElementById("newQuote").onclick = function(){
+	getQuote();
+};
+
+getQuote();
