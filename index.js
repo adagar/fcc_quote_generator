e48d95edbe0317c9e24fa61264b57870070969ab
Adalberto "Nick" Garza <angarza@alum.mit.edu>
2018-03-15 14:36:59 -0400

    Added random background generator

--- index.js
+++ index.js
@@ -9,12 +9,31 @@ function getQuote () {
 			console.log(data);
 			document.getElementById("insertQuote").innerHTML = data.quote;
 			document.getElementById("insertAuthor").innerHTML = data.author;
+			document.getElementBy
 	}
 });
 }
 
+function genRandBGCol(){
+	var x = Math.floor(Math.random() * 256);
+	var y = Math.floor(Math.random() * 256);
+	var z = Math.floor(Math.random() * 256);
+	var bgColor = "rgb(" + x + ", " + y + ", " + z + ")";
+	
+	var textX = Math.abs(256 - x);
+	var textY = Math.abs(256 - y);
+	var textZ = Math.abs(256 - z);
+	var textColor = "rgb(" + textX + ", " + textY + ", " + textZ + ")";
+
+	console.log(bgColor);
+	document.body.style.background = bgColor;
+	document.body.style.color = textColor;
+}
+
 document.getElementById("newQuote").onclick = function(){
 	getQuote();
+	genRandBGCol();
 };
 
 getQuote();
+genRandBGCol();
